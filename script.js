6f52a02bf4886273372db3968e0fc8ad9062683b
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-06-16 21:25:27 +0300

    basic quiz version

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const quizData = [
 		b: 'Donald Trump',
 		c: 'Ivan Saldano',
 		d: 'Joe Biden',
-		correct: 'b',
+		correct: 'd',
 	},
 	{
 		question: 'What does HTML stand for?',
@@ -41,6 +41,8 @@ const quizData = [
 	},
 ]
 
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
 const questionEl = document.getElementById('question')
 const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
@@ -49,15 +51,52 @@ const d_text = document.getElementById('d_text')
 const sumbitBtn = document.getElementById('submit')
 
 let currentQuiz = 0
+let score = 0
 
 loadQuiz()
 
 function loadQuiz() {
+	deselectAnswers()
 	const currentQuizData = quizData[currentQuiz]
 	questionEl.innerText = currentQuizData.question
 	a_text.innerText = currentQuizData.a
 	b_text.innerText = currentQuizData.b
 	c_text.innerText = currentQuizData.c
 	d_text.innerText = currentQuizData.d
-	currentQuestion++
 }
+
+function getSelected() {
+	let answer = undefined
+
+	answerEls.forEach(answerEl => {
+		if (answerEl.checked) {
+			answer = answerEl.id
+		}
+	})
+
+	return answer
+}
+
+function deselectAnswers() {
+	answerEls.forEach(answerEl => {
+		answerEl.checked = false
+	})
+}
+
+sumbitBtn.addEventListener('click', () => {
+	// check to see the answer
+	const answer = getSelected()
+
+	if (answer) {
+		if (answer === quizData[currentQuiz].correct) {
+			score++
+		}
+
+		currentQuiz++
+		if (currentQuiz < quizData.length) {
+			loadQuiz()
+		} else {
+			quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2>`
+		}
+	}
+})
